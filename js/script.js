18b076bc5e9cc38097796f976a967028bf652d81
Shaginur <shaginu@gmail.com>
2023-12-07 00:15:26 +0500

    начал блок uber

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-const open = document.getElementById('hamburger')
-const clouse = document.getElementById('clouse')
+const hamburger = document.querySelector('#hamburger')
+const modal = document.querySelector('#modal')
+const clouse = document.querySelector('#clouse')
 
-open.onclick = function () {
-  return (open.style.display = block)
-}
+hamburger.addEventListener('click', (e) => {
+  let path = e.currentTarget.querySelector('.nav-modal')
+  modal.style.display = 'block'
+})
+
+clouse.addEventListener('click', (e) => {
+  let path = e.currentTarget.querySelector('.clouse')
+  modal.style.display = 'none'
+})
